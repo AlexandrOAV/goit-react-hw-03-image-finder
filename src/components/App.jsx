a1cf3147f63937bb7ad5f370f443ca2a6ca174ab
AlexandrOAV <a0617011995@gmail.com>
2023-07-28 02:00:31 +0300

    280723-0200

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,43 @@
 // import { Audio } from 'react-loader-spinner'
 
+// import axios from 'axios';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { Component } from "react";
+import { ImageGallery } from "./ImageGallery/ImageGallery";
 import { Searchbar } from "./Searchbar/Searchbar";
 
-export const App = () => {
-  return (
-    <div>
-      <Searchbar/>
+// const KEY = '34310450-164638d0ced594f3db31885e7';
+// const URL = 'https://pixabay.com/api/';
+
+export class App extends Component {
+  state = {
+   textSerch:'',
+  //  id: '',
+  //  webformatURL: '', //посилання на маленьке зображення для списку карток
+  //  largeImageURL: '', //посилання на велике зображення для модального вікна
+  //  page: 1,
+ }
+  componentDidMount() {
+   
+    //  this.setState({image:'rambo'})
+    //  axios.get(URL, {params:{key:KEY, q:this.state.image, page:1, per_page:12}}).then(response => {
+    //   console.log(response.data.hits)
+    // }).catch(error=>{  console.log('error: ', error);})
+  
+  }
+  hendleSerchSubmit=(imageSerch)=>{
+    this.setState({textSerch:imageSerch})
+  }
+  render() {
+console.log('imageSerch: ', this.state.textSerch);
+       return (
+         <div>
+            <ToastContainer/>
+           <Searchbar onSubmit={this.hendleSerchSubmit} />
+           <ImageGallery position="top-center" autoClose={2000} />
+          
     </div>
   );
+  }
 };
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,38 +1,52 @@
 import { Formik, Form, Field } from 'formik';
 import css from './Searchbar.module.css'
 import { BsSearch } from 'react-icons/bs'
+import { Component } from 'react';
+ import { toast } from 'react-toastify';
 
 
+export class Searchbar extends Component {
+  state = {
+   values:'',
+  }
+  
+  submitSerchbar = (values, actions) => {
+    this.setState({ values: values });
+    if (values.serchText.trim() === '') {
+      return toast.error('Please enter a query to search',
+                          {position: "top-center",
+                           autoClose: 2000,
+                          })
+    } 
+    this.props.onSubmit(values);
+    actions.resetForm()
+  }
 
-export const Searchbar = () => {
-    const submitSerchbar = (values, actions) => {
-        console.log('values: ', values);
-        console.log('actions', actions);
-        actions.resetForm()
-    }
+  render() {
+    
     return (
- <header className={css.Searchbar}>
+      <header className={css.Searchbar}>
         <Formik
           initialValues={{ serchText: '' }}
-         
-          onSubmit={submitSerchbar}
+          onSubmit={this.submitSerchbar}
         >
-  <Form className={css.SearchForm}>
-    <button type="submit" className={css.SearchForm_button}>
-    <BsSearch className={ css.SearchForm_button_label } /> 
-    </button>
-     <Field
-      name='serchText'
-      className={css.SearchForm_input}
-      type="text"
-      autoComplete="off"
-      autoFocus
-      pattern="[A-Za-z0-9 ]*"
-     placeholder="Search images and photos"
+          <Form className={css.SearchForm}>
+            <button type="submit" className={css.SearchForm_button}>
+              <BsSearch className={css.SearchForm_button_label} />
+            </button>
+            <Field
+              name='serchText'
+              className={css.SearchForm_input}
+              type="text"
+              autoComplete="off"
+              autoFocus
+              pattern="[A-Za-z0-9 ]*"
+              placeholder="Search images and photos"
             />
             
-  </Form>
-</Formik>
-</header>
+          </Form>
+        </Formik>
+      </header>
     )
+  }
 }
