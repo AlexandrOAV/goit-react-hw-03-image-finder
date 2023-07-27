e06c596611c21c7ab6c1d9d0f8185b3a2846b6d8
AlexandrOAV <a0617011995@gmail.com>
2023-07-27 21:44:21 +0300

    form end

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -2,6 +2,8 @@ import { Formik, Form, Field } from 'formik';
 import css from './Searchbar.module.css'
 import { BsSearch } from 'react-icons/bs'
 
+
+
 export const Searchbar = () => {
     const submitSerchbar = (values, actions) => {
         console.log('values: ', values);
@@ -10,20 +12,25 @@ export const Searchbar = () => {
     }
     return (
  <header className={css.Searchbar}>
-    <Formik initialValues={{ name: '' }} onSubmit={submitSerchbar}>
+        <Formik
+          initialValues={{ serchText: '' }}
+         
+          onSubmit={submitSerchbar}
+        >
   <Form className={css.SearchForm}>
     <button type="submit" className={css.SearchForm_button}>
     <BsSearch className={ css.SearchForm_button_label } /> 
     </button>
      <Field
-      name='name'
+      name='serchText'
       className={css.SearchForm_input}
       type="text"
       autoComplete="off"
       autoFocus
+      pattern="[A-Za-z0-9 ]*"
      placeholder="Search images and photos"
-     
-    />
+            />
+            
   </Form>
 </Formik>
 </header>
